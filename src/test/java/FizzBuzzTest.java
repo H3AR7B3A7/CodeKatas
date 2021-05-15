7dbaa7f3d185acb1ad750c5e8aa22d31f714222f
Steven <steven.dhondt@kenze.be>
2021-05-15 14:13:19 +0200

    :sparkles: FizzBuzz Kata

--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -5,37 +5,37 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class FizzBuzzTest {
 
     @Test
-    public void of_0is0() {
+    void of_0is0() {
         assertEquals(FizzBuzz.of(0), "0");
     }
 
     @Test
-    public void of_1is1() {
+    void of_1is1() {
         assertEquals(FizzBuzz.of(1), "1");
     }
 
     @Test
-    public void of_3isFizz() {
+    void of_3isFizz() {
         assertEquals(FizzBuzz.of(3), "Fizz");
     }
 
     @Test
-    public void of_5isBuzz() {
+    void of_5isBuzz() {
         assertEquals(FizzBuzz.of(5), "Buzz");
     }
 
     @Test
-    public void of_6isFizz() {
+    void of_6isFizz() {
         assertEquals(FizzBuzz.of(6), "Fizz");
     }
 
     @Test
-    public void of_10isBuzz() {
+    void of_10isBuzz() {
         assertEquals(FizzBuzz.of(10), "Buzz");
     }
 
     @Test
-    public void of_15isFizzBuzz() {
+    void of_15isFizzBuzz() {
         assertEquals(FizzBuzz.of(15), "FizzBuzz");
     }
 }
