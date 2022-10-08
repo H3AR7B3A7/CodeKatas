cbbddd65bb914424835e64bfad016b95a48f6a88
H3AR7B3A7 <steven.d.hondt.sdh@gmail.com>
2022-10-08 07:50:04 +0200

    :ambulance: Handle null values in GildedRose constructor

--- refactoring-katas/gilded-rose-kata-finished/src/main/java/com/gildedrose/GildedRose.java
+++ refactoring-katas/gilded-rose-kata-finished/src/main/java/com/gildedrose/GildedRose.java
@@ -4,16 +4,20 @@ import com.gildedrose.storeItems.StoreItem;
 import com.gildedrose.storeItems.StoreItemFactory;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 public final class GildedRose {
     private final List<StoreItem> items;
 
     public GildedRose(Item... items) {
-        this.items = Arrays.stream(items)
-            .map(item -> StoreItemFactory.get(item.name, item.sellIn, item.quality))
-            .collect(Collectors.toList());
+        this.items = items == null ? Collections.emptyList() :
+            Arrays.stream(items)
+                .filter(Objects::nonNull)
+                .map(item -> StoreItemFactory.get(item.name, item.sellIn, item.quality))
+                .collect(Collectors.toList());
     }
 
     public Item[] getItems() {
