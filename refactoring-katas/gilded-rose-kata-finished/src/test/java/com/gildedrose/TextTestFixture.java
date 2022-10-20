7d633728b834bb97f4b8b6ec737f09a49120b432
H3AR7B3A7 <steven.d.hondt.sdh@gmail.com>
2022-10-20 03:10:47 +0200

    :alembic: Some API play

--- refactoring-katas/gilded-rose-kata-finished/src/test/java/com/gildedrose/TextTestFixture.java
+++ refactoring-katas/gilded-rose-kata-finished/src/test/java/com/gildedrose/TextTestFixture.java
@@ -1,14 +1,16 @@
 package com.gildedrose;
 
+import java.util.Arrays;
+
 public class TextTestFixture {
     public static void main(String[] args) {
         System.out.println("OMGHAI!");
 
         Item[] items = new Item[] {
-                new Item("+5 Dexterity Vest", 10, 20), //
-                new Item("Aged Brie", 2, 0), //
-                new Item("Elixir of the Mongoose", 5, 7), //
-                new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
+                new Item("+5 Dexterity Vest", 10, 20),
+                new Item("Aged Brie", 2, 0),
+                new Item("Elixir of the Mongoose", 5, 7),
+                new Item("Sulfuras, Hand of Ragnaros", 0, 80),
                 new Item("Sulfuras, Hand of Ragnaros", -1, 80),
                 new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
                 new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
@@ -34,10 +36,10 @@ public class TextTestFixture {
         }
 
         /**
-         * Some more API play
+         * API play with conjured special items
          */
         Item[] brie = new Item[] {
-            new Item("Aged Brie", 5, 5), //
+            new Item("Aged Brie", 5, 5),
             new Item("Conjured Aged Brie", 5, 5) };
 
         GildedRose cheeseShop = new GildedRose(brie);
@@ -51,5 +53,23 @@ public class TextTestFixture {
             System.out.println();
             cheeseShop.updateQuality();
         }
+
+        /**
+         * API play with conjured item shops convenience methods,
+         * and conjured item name mapping
+         */
+
+        Item[] regularItems = new Item[] {
+            new Item("Robe", 5, 5),
+            new Item("Staff", 5, 5) };
+
+        GildedRose mageShop = new GildedRose(regularItems);
+
+        mageShop.setAllConjured(true);
+        Arrays.stream(mageShop.getItems()).forEach(System.out::println);
+        mageShop.setAllConjured(false);
+        Arrays.stream(mageShop.getItems()).forEach(System.out::println);
+        mageShop.setConjured(0, true);
+        Arrays.stream(mageShop.getItems()).forEach(System.out::println);
     }
 }
